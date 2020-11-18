6d6592e16023c9a5265291174b23c52928ff81d9
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-11-18 10:28:53 +0900

    First commit.

---
+++ Assets/Scripts/ActivateByCollisionAndCount.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateByCollisionAndCount : MonoBehaviour
+{
+    public GameObject[] flowchatrs;
+    public int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if ( collision.gameObject.tag == "Player")
+        {
+            count++;
+            if ( count >= flowchatrs.Length)
+            {
+                flowchatrs[0].SetActive(true);
+            }
+            else
+            {
+                flowchatrs[count].SetActive(true);
+            }
+        }
+    }
+}
